cc082b53ef8402e9a73d846aeffe7530c075aa76
k0 <21180271+k073l@users.noreply.github.com>
2025-09-11 03:07:52 +0200

    feat: custom console command creation

---
+++ S1API/Console/BaseConsoleCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace S1API.Console
+{
+    /// <summary>
+    /// Abstract base class for creating console commands.
+    /// </summary>
+    public abstract class BaseConsoleCommand
+    {
+        /// <summary>
+        /// The command word that triggers this console command.
+        /// </summary>
+        public abstract string CommandWord { get; }
+
+        /// <summary>
+        /// A brief description of what the command does.
+        /// </summary>
+        public abstract string CommandDescription { get; }
+
+        /// <summary>
+        /// An example of how to use the command.
+        /// </summary>
+        public abstract string ExampleUsage { get; }
+
+        /// <summary>
+        /// Executes the command with the provided arguments.
+        /// </summary>
+        /// <param name="args">The list of arguments passed to the command.</param>
+        public abstract void ExecuteCommand(List<string> args);
+    }
+}
