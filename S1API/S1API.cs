2771fe05ca03bea599ac71a9c10ae2d35642b28e
ifBars <tristens5465@gmail.com>
2025-09-14 00:53:16 -0700

    Remove BepInEx

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -1,5 +1,4 @@
-﻿#if (MONOMELON || IL2CPPMELON)
-using MelonLoader;
+﻿using MelonLoader;
 
 [assembly: MelonInfo(typeof(S1API.S1API), "S1API (Forked by Bars)", "1.8.1", "KaBooMa")]
 
@@ -12,35 +11,3 @@ namespace S1API
     {
     }
 }
-#elif (IL2CPPBEPINEX || MONOBEPINEX)
-using BepInEx;
-
-#if MONOBEPINEX
-using BepInEx.Unity.Mono;
-#elif IL2CPPBEPINEX
-using BepInEx.Unity.IL2CPP;
-#endif
-
-using HarmonyLib;
-
-namespace S1API
-{
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
-    public class S1API : 
-#if MONOBEPINEX
-        BaseUnityPlugin
-#elif IL2CPPBEPINEX
-        BasePlugin
-#endif
-    {
-#if MONOBEPINEX
-        private void Awake()
-#elif IL2CPPBEPINEX
-        public override void Load()
-#endif
-        {
-            new Harmony("com.S1API").PatchAll();
-        }
-    }
-}
-#endif
