f780f3e693063953c393fde849d436bd9833e379
ifBars <tristens5465@gmail.com>
2025-11-09 08:32:37 -0800

    Enhance weapon handling and POI compatibility
    Added methods to CombatBehaviour for setting weapons using Equippable and EquippableBuilder, with improved asset path extraction. Updated QuestEntry to support backward compatibility for POI position setting, enabling automatic POI creation for older mods. Bumped version to 2.5.2.

--- S1API/Entities/Behaviour/CombatBehaviour.cs
+++ S1API/Entities/Behaviour/CombatBehaviour.cs
@@ -9,9 +9,11 @@ using FishNet.Object;
 #endif
 using System.Reflection;
 using S1API.Entities.Interfaces;
+using S1API.Items;
 using UnityEngine;
 using MelonLoader;
 using S1API.Entities.Equippables;
+using Object = UnityEngine.Object;
 
 namespace S1API.Entities.Behaviour;
 
@@ -48,8 +50,8 @@ public class CombatBehaviour
     /// </summary>
     public float GiveUpTime
     {
-        get => NPC.S1NPC.Behaviour.CombatBehaviour.GiveUpTime;
-        set => NPC.S1NPC.Behaviour.CombatBehaviour.GiveUpTime = value;
+        get => NPC.S1NPC.Behaviour.CombatBehaviour.DefaultSearchTime;
+        set => NPC.S1NPC.Behaviour.CombatBehaviour.DefaultSearchTime = value;
     }
 
     /// <summary>
@@ -59,9 +61,19 @@ public class CombatBehaviour
     /// </summary>
     public string DefaultWeaponAssetPath
     {
-        get => NPC.S1NPC.Behaviour.CombatBehaviour.DefaultWeapon.AssetPath;
+        get
+        {
+            var defaultWeapon = NPC.S1NPC.Behaviour.CombatBehaviour.DefaultWeapon;
+            return defaultWeapon?.AssetPath ?? string.Empty;
+        }
         set
         {
+            if (string.IsNullOrEmpty(value))
+            {
+                NPC.S1NPC.Behaviour.CombatBehaviour.DefaultWeapon = null;
+                return;
+            }
+
             var go = Resources.Load(value) as GameObject;
             if (go == null)
             {
@@ -103,5 +115,145 @@ public class CombatBehaviour
 #else
         NPC.S1NPC.Behaviour.CombatBehaviour.SetWeapon(weaponPath);
 #endif
+    }
+
+    /// <summary>
+    /// Sets the current weapon of the NPC using an S1API Equippable wrapper.
+    /// This method extracts the asset path from the equippable's associated AvatarEquippable component.
+    /// </summary>
+    /// <param name="equippable">The S1API Equippable wrapper containing the weapon to equip.</param>
+    /// <remarks>
+    /// This method attempts to find an AvatarEquippable component on the equippable's GameObject
+    /// and uses its AssetPath. If no AvatarEquippable is found, this method will log an error.
+    /// </remarks>
+    public void SetCurrentWeapon(Equippable equippable)
+    {
+        if (equippable == null)
+        {
+            Debug.LogError("Cannot set weapon: Equippable is null");
+            return;
+        }
+
+        var assetPath = GetAssetPathFromEquippable(equippable);
+        if (string.IsNullOrEmpty(assetPath))
+        {
+            Debug.LogError("Cannot set weapon: Could not extract asset path from Equippable");
+            return;
+        }
+
+        SetCurrentWeapon(assetPath);
+    }
+
+    /// <summary>
+    /// Sets the default weapon for the NPC's combat behaviour using an S1API Equippable wrapper.
+    /// This method extracts the asset path from the equippable's associated AvatarEquippable component.
+    /// </summary>
+    /// <param name="equippable">The S1API Equippable wrapper containing the weapon to set as default.</param>
+    /// <remarks>
+    /// This method attempts to find an AvatarEquippable component on the equippable's GameObject
+    /// and uses its AssetPath. If no AvatarEquippable is found, this method will log an error.
+    /// </remarks>
+    public void SetDefaultWeapon(Equippable equippable)
+    {
+        if (equippable == null)
+        {
+            Debug.LogError("Cannot set default weapon: Equippable is null");
+            return;
+        }
+
+        var assetPath = GetAssetPathFromEquippable(equippable);
+        if (string.IsNullOrEmpty(assetPath))
+        {
+            Debug.LogError("Cannot set default weapon: Could not extract asset path from Equippable");
+            return;
+        }
+
+        DefaultWeaponAssetPath = assetPath;
+    }
+
+    /// <summary>
+    /// Sets the current weapon of the NPC using an EquippableBuilder-created equippable.
+    /// This method extracts the asset path from the builder's AvatarEquippable configuration.
+    /// </summary>
+    /// <param name="equippableBuilder">The EquippableBuilder instance containing the weapon configuration.</param>
+    /// <remarks>
+    /// This method requires the EquippableBuilder to have been configured with WithAvatarEquippable()
+    /// to provide an asset path. The builder will be built automatically if not already built.
+    /// </remarks>
+    public void SetCurrentWeapon(EquippableBuilder equippableBuilder)
+    {
+        if (equippableBuilder == null)
+        {
+            Debug.LogError("Cannot set weapon: EquippableBuilder is null");
+            return;
+        }
+
+        // Build the equippable to get the GameObject
+        var equippable = equippableBuilder.Build();
+        SetCurrentWeapon(equippable);
+    }
+
+    /// <summary>
+    /// Sets the default weapon for the NPC's combat behaviour using an EquippableBuilder-created equippable.
+    /// This method extracts the asset path from the builder's AvatarEquippable configuration.
+    /// </summary>
+    /// <param name="equippableBuilder">The EquippableBuilder instance containing the weapon configuration.</param>
+    /// <remarks>
+    /// This method requires the EquippableBuilder to have been configured with WithAvatarEquippable()
+    /// to provide an asset path. The builder will be built automatically if not already built.
+    /// </remarks>
+    public void SetDefaultWeapon(EquippableBuilder equippableBuilder)
+    {
+        if (equippableBuilder == null)
+        {
+            Debug.LogError("Cannot set default weapon: EquippableBuilder is null");
+            return;
+        }
+
+        // Build the equippable to get the GameObject
+        var equippable = equippableBuilder.Build();
+        SetDefaultWeapon(equippable);
+    }
+
+    /// <summary>
+    /// INTERNAL: Extracts the asset path from an S1API Equippable wrapper by finding its associated AvatarEquippable component.
+    /// </summary>
+    /// <param name="equippable">The S1API Equippable wrapper.</param>
+    /// <returns>The asset path if found, otherwise null or empty string.</returns>
+    private string GetAssetPathFromEquippable(Equippable equippable)
+    {
+        if (equippable?.S1Equippable == null)
+        {
+            return null;
+        }
+
+        // Try to get the GameObject from the equippable component
+        var gameObject = equippable.S1Equippable.gameObject;
+        if (gameObject == null)
+        {
+            return null;
+        }
+
+        // Look for AvatarEquippable component on the GameObject or its children
+        var avatarEquippable = gameObject.GetComponent<AvatarEquippable>();
+        if (avatarEquippable == null)
+        {
+            avatarEquippable = gameObject.GetComponentInChildren<AvatarEquippable>();
+        }
+
+        // If still not found, check if there's a child GameObject with AvatarEquippable
+        if (avatarEquippable == null)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                avatarEquippable = child.GetComponent<AvatarEquippable>();
+                if (avatarEquippable != null)
+                {
+                    break;
+                }
+            }
+        }
+
+        return avatarEquippable?.AssetPath;
     }
 }
