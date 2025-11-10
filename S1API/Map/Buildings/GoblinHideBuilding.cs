40cb124c14e2102438c30899a3162ea8735b6dfb
ifBars <tristens5465@gmail.com>
2025-11-09 17:00:55 -0800

    Create GoblinHideBuilding.cs

---
+++ S1API/Map/Buildings/GoblinHideBuilding.cs
@@ -0,0 +1,10 @@
+namespace S1API.Map.Buildings
+{
+	/// <summary>
+	/// Identifier for the base-game building named "Goblin Hide Building".
+	/// Modders can use <see cref="Building.Get{GoblinHideBuilding}()"/> to resolve it.
+	/// </summary>
+	[BuildingName("Goblin Hide Building")]
+	public sealed class GoblinHideBuilding : IBuildingIdentifier { }
+}
+
