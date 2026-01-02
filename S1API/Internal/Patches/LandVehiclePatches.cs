33920a4335566206a09c137dcc34e12696c0a8e7
LilithMotherOfAll <33379004+Spectre0987@users.noreply.github.com>
2026-01-02 09:04:37 -0800

    Made OnDestroy remove the vehicle from the game's lists. Stopps save errors and vehicles returning after a reload when destroyed

---
+++ S1API/Internal/Patches/LandVehiclePatches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using S1API.Vehicles;
+
+#if (IL2CPPMELON || Il2CppBepInEx)
+using S1Vehicles = Il2CppScheduleOne.Vehicles;
+#else
+using S1Vehicles = ScheduleOne.Vehicles;
+#endif
+
+namespace S1API.Internal.Patches
+{
+    [HarmonyPatch(typeof(S1Vehicles.LandVehicle))]
+    internal class LandVehiclePatches
+    {
+        [HarmonyPatch("OnDestroy")]
+        [HarmonyPostfix]
+        public static void OnDestroy(S1Vehicles.LandVehicle __instance) {
+            VehicleRegistry.RemoveVehicle(__instance.GUID.ToString());
+        }
+
+    }
+}
