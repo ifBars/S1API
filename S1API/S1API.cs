80155d17b4c96068b895af5ef88d317d81c8cf23
ifBars <tristens5465@gmail.com>
2026-01-07 18:22:47 -0800

    v2.9.2

--- S1API/S1API.cs
+++ S1API/S1API.cs
@@ -1,10 +1,13 @@
+using System;
 using MelonLoader;
 using S1API.Internal;
 using S1API.Internal.Lifecycle;
 using S1API.Lifecycle;
 using S1API.Map;
 
-[assembly: MelonInfo(typeof(S1API.S1API), "S1API (Forked by Bars)", "2.9.1", "KaBooMa")]
+[assembly: MelonInfo(typeof(S1API.S1API), "S1API (Forked by Bars)", "2.9.2", "KaBooMa")]
+[assembly: MelonPriority(Int32.MinValue)]
+[assembly: MelonIncompatibleAssemblies("ModManager&PhoneApp", "AutoClearCompletedDeals")]
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace S1API
 {
