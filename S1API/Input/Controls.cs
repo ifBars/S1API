710213be1a93e9fb2969214396ec46d229712388
ifBars <tristens5465@gmail.com>
2026-02-09 19:41:12 -0800

    feat: Better Controls & ProductManager Wrapping

--- S1API/Input/Controls.cs
+++ S1API/Input/Controls.cs
@@ -1,29 +1,184 @@
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 #if (IL2CPPMELON)
 using S1GameInput = Il2CppScheduleOne.GameInput;
+using S1ButtonCode = Il2CppScheduleOne.GameInput.ButtonCode;
+using S1InputDeviceType = Il2CppScheduleOne.GameInput.InputDeviceType;
 #elif (MONOMELON || MONOBEPINEX || IL2CPPBEPINEX)
 using S1GameInput = ScheduleOne.GameInput;
+using S1ButtonCode = ScheduleOne.GameInput.ButtonCode;
+using S1InputDeviceType = ScheduleOne.GameInput.InputDeviceType;
 #endif
 
 namespace S1API.Input
 {
-	/// <summary>
-	/// Modder-facing facade over the base game's input state to keep S1API consumers decoupled
-	/// from the underlying <c>ScheduleOne.GameInput</c> type across Mono/IL2CPP.
-	/// </summary>
-	public static class Controls
-	{
-		/// <summary>
-		/// Gets or sets whether the player is currently typing in a UI field.
-		/// When true, gameplay input should generally be ignored by systems listening for controls.
-		/// </summary>
-		public static bool IsTyping
-		{
-			get => S1GameInput.IsTyping;
-			set => S1GameInput.IsTyping = value;
-		}
-	}
+    /// <summary>
+    /// Button codes for all game actions that can be checked via GetButton methods.
+    /// Mirrors ScheduleOne.GameInput.ButtonCode for cross-platform compatibility.
+    /// </summary>
+    public enum ButtonCode
+    {
+        PrimaryClick = 0,
+        SecondaryClick = 1,
+        TertiaryClick = 2,
+        Forward = 3,
+        Backward = 4,
+        Left = 5,
+        Right = 6,
+        Jump = 7,
+        Crouch = 8,
+        Sprint = 9,
+        Escape = 10,
+        Back = 11,
+        Interact = 12,
+        Submit = 13,
+        TogglePhone = 14,
+        VehicleToggleLights = 15,
+        VehicleHandbrake = 16,
+        RotateLeft = 17,
+        RotateRight = 18,
+        ManagementMode = 19,
+        OpenMap = 20,
+        OpenJournal = 21,
+        OpenTexts = 22,
+        QuickMove = 23,
+        ToggleFlashlight = 24,
+        ViewAvatar = 25,
+        Reload = 26,
+        InventoryLeft = 27,
+        InventoryRight = 28,
+        Holster = 29,
+        VehicleResetCamera = 30,
+        SkateboardDismount = 31,
+        SkateboardMount = 32
+    }
+
+    /// <summary>
+    /// Types of input devices supported by the game.
+    /// Mirrors ScheduleOne.GameInput.InputDeviceType for cross-platform compatibility.
+    /// </summary>
+    public enum InputDeviceType
+    {
+        KeyboardMouse = 0,
+        Gamepad = 1
+    }
+
+    /// <summary>
+    /// INTERNAL: Stores the last known input device for change detection.
+    /// </summary>
+    internal static class ControlsState
+    {
+        internal static InputDeviceType LastKnownDevice;
+        internal static readonly Dictionary<Action<InputDeviceType>, Action<S1InputDeviceType>> TrackedListeners 
+            = new Dictionary<Action<InputDeviceType>, Action<S1InputDeviceType>>();
+    }
+
+    /// <summary>
+    /// Modder-facing facade over the base game's input state to keep S1API consumers decoupled
+    /// from the underlying <c>ScheduleOne.GameInput</c> type across Mono/IL2CPP.
+    /// </summary>
+    public static class Controls
+    {
+        /// <summary>
+        /// Gets or sets whether the player is currently typing in a UI field.
+        /// When true, gameplay input should generally be ignored by systems listening for controls.
+        /// </summary>
+        public static bool IsTyping
+        {
+            get => S1GameInput.IsTyping;
+            set => S1GameInput.IsTyping = value;
+        }
+
+        /// <summary>
+        /// Gets the current motion input axis (WASD / left analog stick).
+        /// X component represents left/right movement, Y component represents forward/backward.
+        /// </summary>
+        public static Vector2 MotionAxis => S1GameInput.MotionAxis;
+
+        /// <summary>
+        /// Gets the current camera/mouse delta input.
+        /// This represents how much the camera has moved this frame.
+        /// </summary>
+        public static Vector2 CameraAxis => S1GameInput.CameraAxis;
+
+        /// <summary>
+        /// Gets the current mouse scroll wheel delta.
+        /// Positive values indicate scrolling up, negative values indicate scrolling down.
+        /// </summary>
+        public static float MouseScrollDelta => S1GameInput.MouseWheelAxis;
+
+        /// <summary>
+        /// Gets the currently active input device type (Keyboard/Mouse or Gamepad).
+        /// </summary>
+        public static InputDeviceType CurrentInputDevice =>
+            (InputDeviceType)(int)S1GameInput.CurrentInputDevice;
+
+        /// <summary>
+        /// Gets whether the controller combo modifier is currently active.
+        /// Used for controller-specific input combinations.
+        /// </summary>
+        public static bool ControllerComboActive => S1GameInput.ControllerComboActive;
+
+        /// <summary>
+        /// Registers a callback for when the active input device changes (e.g., from keyboard to gamepad).
+        /// The callback receives the new input device type.
+        /// </summary>
+        /// <param name="callback">The callback to invoke when input device changes.</param>
+        public static void RegisterDeviceChangedListener(Action<InputDeviceType> callback)
+        {
+            if (callback == null)
+                return;
+
+            if (ControlsState.TrackedListeners.ContainsKey(callback))
+                return;
+
+            Action<S1InputDeviceType> wrapper = (device) =>
+                callback((InputDeviceType)(int)device);
+
+            ControlsState.TrackedListeners[callback] = wrapper;
+            S1GameInput.OnInputDeviceChanged += wrapper;
+        }
+
+        /// <summary>
+        /// Unregisters a callback from input device change events.
+        /// </summary>
+        /// <param name="callback">The callback to remove.</param>
+        public static void UnregisterDeviceChangedListener(Action<InputDeviceType> callback)
+        {
+            if (callback == null)
+                return;
+
+            if (!ControlsState.TrackedListeners.TryGetValue(callback, out Action<S1InputDeviceType>? wrapper))
+                return;
+
+            S1GameInput.OnInputDeviceChanged -= wrapper;
+            ControlsState.TrackedListeners.Remove(callback);
+        }
+
+        /// <summary>
+        /// Checks if the specified button is currently held down.
+        /// </summary>
+        /// <param name="button">The button to check.</param>
+        /// <returns>True if the button is currently held down.</returns>
+        public static bool GetButton(ButtonCode button) =>
+            S1GameInput.GetButton((S1ButtonCode)(int)button);
+
+        /// <summary>
+        /// Checks if the specified button was pressed down this frame.
+        /// </summary>
+        /// <param name="button">The button to check.</param>
+        /// <returns>True if the button was pressed this frame.</returns>
+        public static bool GetButtonDown(ButtonCode button) =>
+            S1GameInput.GetButtonDown((S1ButtonCode)(int)button);
+
+        /// <summary>
+        /// Checks if the specified button was released this frame.
+        /// </summary>
+        /// <param name="button">The button to check.</param>
+        /// <returns>True if the button was released this frame.</returns>
+        public static bool GetButtonUp(ButtonCode button) =>
+            S1GameInput.GetButtonUp((S1ButtonCode)(int)button);
+    }
 }
-
-
--- S1API/Products/ProductManager.cs
+++ S1API/Products/ProductManager.cs
@@ -1,6 +1,5 @@
-﻿#if (IL2CPPMELON)
+#if (IL2CPPMELON)
 using S1Product = Il2CppScheduleOne.Product;
-using Il2CppSystem.Collections.Generic;
 #elif (MONOMELON || MONOBEPINEX || IL2CPPBEPINEX)
 using S1Product = ScheduleOne.Product;
 #endif
@@ -14,11 +13,70 @@ namespace S1API.Products
     public static class ProductManager
     {
         /// <summary>
+        /// Minimum price for any product (1).
+        /// </summary>
+        public const int MinPrice = 1;
+
+        /// <summary>
+        /// Maximum price for any product (999).
+        /// </summary>
+        public const int MaxPrice = 999;
+
+        /// <summary>
         /// A list of product definitions discovered on this save.
         /// </summary>
         public static ProductDefinition[] DiscoveredProducts => S1Product.ProductManager.DiscoveredProducts.ToArray()
             .Select(productDefinition => ProductDefinitionWrapper.Wrap(new ProductDefinition(productDefinition)))
             .ToArray();
 
+        /// <summary>
+        /// A list of products currently listed for sale.
+        /// </summary>
+        public static ProductDefinition[] ListedProducts => S1Product.ProductManager.ListedProducts.ToArray()
+            .Select(productDefinition => ProductDefinitionWrapper.Wrap(new ProductDefinition(productDefinition)))
+            .ToArray();
+
+        /// <summary>
+        /// A list of favourited products.
+        /// </summary>
+        public static ProductDefinition[] FavouritedProducts => S1Product.ProductManager.FavouritedProducts.ToArray()
+            .Select(productDefinition => ProductDefinitionWrapper.Wrap(new ProductDefinition(productDefinition)))
+            .ToArray();
+
+        /// <summary>
+        /// Gets whether the player is currently accepting orders.
+        /// </summary>
+        public static bool IsAcceptingOrders => S1Product.ProductManager.IsAcceptingOrders;
+
+        /// <summary>
+        /// Gets whether meth has been discovered.
+        /// </summary>
+        public static bool MethDiscovered => S1Product.ProductManager.MethDiscovered;
+
+        /// <summary>
+        /// Gets whether cocaine has been discovered.
+        /// </summary>
+        public static bool CocaineDiscovered => S1Product.ProductManager.CocaineDiscovered;
+
+        /// <summary>
+        /// Gets whether shrooms have been discovered.
+        /// </summary>
+        public static bool ShroomsDiscovered => S1Product.ProductManager.ShroomsDiscovered;
+
+        /// <summary>
+        /// Gets the current price of a product.
+        /// </summary>
+        /// <param name="product">The product definition.</param>
+        /// <returns>The price of the product.</returns>
+        public static float GetPrice(ProductDefinition product) => S1Product.ProductManager.Instance.GetPrice(product.S1ProductDefinition);
+
+        /// <summary>
+        /// Calculates the value of a product based on its properties.
+        /// </summary>
+        /// <param name="product">The product definition.</param>
+        /// <param name="baseValue">The base value to calculate from.</param>
+        /// <returns>The calculated value.</returns>
+        public static float CalculateProductValue(ProductDefinition product, float baseValue) =>
+            S1Product.ProductManager.CalculateProductValue(product.S1ProductDefinition, baseValue);
     }
 }
